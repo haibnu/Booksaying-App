238367f94d9adf54dbac8b3ac93fb1214bbfe394
Halil <halilibrahim@nuroglu.net>
2017-03-26 20:18:28 +0300

    resimler tekrar optimze edildi. kodlar ios.js'e de aktarıldı

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,67 @@
 /**
- * Sample React Native App
+ * Booksaying App with React Native
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
+
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Navigator
 } from 'react-native';
 
+import Welcome from './src/components/Welcome/Welcome';
+import Login from './src/components/login';
+import Register from './src/components/register';
+
 export default class bookSaying extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+	renderScene(route, navigator){
+		var routeId = route.id;
+		if(routeId === 'Login'){
+			return <Login navigator={navigator}/>;
+		}
+		if(routeId === 'Register'){
+			return <Register navigator={navigator}/>;
+		}
+		if(routeId === 'Welcome'){
+			return <Welcome navigator={navigator}/>;
+		}
+	}
+
+	render() {
+		return(
+			<Navigator
+					initialRoute={{
+						id: 'Welcome',
+						name: 'Welcome'
+					}}
+					renderScene={
+						this.renderScene.bind(this)
+					}
+			/>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+	 flex: 1,
+	 justifyContent: 'center',
+	 alignItems: 'center',
+	 backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+	 fontSize: 20,
+	 textAlign: 'center',
+	 margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+	 textAlign: 'center',
+	 color: '#333333',
+	 marginBottom: 5,
   },
 });
 
